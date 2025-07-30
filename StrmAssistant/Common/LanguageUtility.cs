c460f089d475e8bc822fa2c27c8839c3ad9f455a
sjtuross <sjtuross@msn.com>
2025-07-30 10:24:08 +0800

    optimize: update default japanese episode name regex

--- StrmAssistant/Common/LanguageUtility.cs
+++ StrmAssistant/Common/LanguageUtility.cs
@@ -15,7 +15,8 @@ namespace StrmAssistant.Common
         private static readonly Regex KoreanRegex = new Regex(@"[\uAC00-\uD7A3]", RegexOptions.Compiled);
         private static readonly Regex DefaultEnglishEpisodeNameRegex = new Regex(@"^Episode\s*\d+$", RegexOptions.Compiled);
         private static readonly Regex DefaultChineseEpisodeNameRegex = new Regex(@"^第\s*\d+\s*集$", RegexOptions.Compiled);
-        private static readonly Regex DefaultJapaneseEpisodeNameRegex = new Regex(@"^第\s*\d+\s*話$", RegexOptions.Compiled);
+        private static readonly Regex DefaultJapaneseEpisodeNameRegex =
+            new Regex(@"^(第\s*\d+\s*話|エピソード\s*\d+)$", RegexOptions.Compiled);
         private static readonly Regex DefaultChineseCollectionNameRegex = new Regex(@"（系列）$", RegexOptions.Compiled);
         private static readonly Regex CleanPersonNameRegex = new Regex(@"\s+", RegexOptions.Compiled);
         private static readonly Regex CleanEpisodeNameRegex =
